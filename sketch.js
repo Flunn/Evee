86e9e63fc2cbd8a95e55414bf69a4d4fb393fef9
Flunn <93621757+Flunn@users.noreply.github.com>
2021-11-02 16:16:01 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+function preload(){
+  //teapot=loadModel("https://raw.githubusercontent.com/processing/p5.js/main/test/manual-test-examples/webgl/assets/teapot-ascii.stl",true);
+  teapot=loadModel("evee_lowpoly_flowalistik.stl",true);
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight,WEBGL);
+}
+
+function draw() {
+  background(120);
+  push();
+  translate(-width/2,-height/2);
+  fill(255,0,0);
+  ellipse(mouseX,mouseY,100,100);
+  pop();
+  push();
+  scale(3);
+  rotateX(PI/2);
+  rotateY(frameCount*0.005);
+  rotateX(frameCount*0.005);
+  noStroke();
+  //ambientLight(100);
+  directionalLight(0,0,255,0,1,0);
+  model(teapot);
+  pop()
+  normalMaterial();
